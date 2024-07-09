144370e9486dea384a0f59688c03bafc53bdf84e
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-07-09 13:09:59 -0300

    added load balancer in question client

--- QuizService/src/main/java/com/quiz/services/QuestionClient.java
+++ QuizService/src/main/java/com/quiz/services/QuestionClient.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.quiz.entities.Question;
 
 //automatic implementation of the interface
-@FeignClient(url = "http://localhost:8082", value = "Question-Client")
+// @FeignClient(url = "http://localhost:8082", value = "Question-Client")
+@FeignClient(name = "QuestionService")
 public interface QuestionClient {
     @GetMapping("/question/quiz/{id}")
     List<Question> getQuestionsByQuiz(@PathVariable Long id);
